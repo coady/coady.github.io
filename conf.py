f92cc3a830c9d27104434d9b434e9680a12c18c0
Aric Coady <aric.coady@gmail.com>
2020-10-24 16:38:13 -0700

    Moved source and deploy branches.

--- conf.py
+++ conf.py
@@ -645,8 +645,8 @@ REDIRECTIONS = []
 # For more details, read the manual:
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
-GITHUB_SOURCE_BRANCH = 'src'
-GITHUB_DEPLOY_BRANCH = 'master'
+GITHUB_SOURCE_BRANCH = 'main'
+GITHUB_DEPLOY_BRANCH = 'gh-pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
 GITHUB_REMOTE_NAME = 'origin'
